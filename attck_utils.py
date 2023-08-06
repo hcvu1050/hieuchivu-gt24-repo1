c015426b5f7f74450183e691af1310028f471778
HieuCVu_1050 <43056834+Hieu1050@users.noreply.github.com>
2023-08-06 18:18:34 +0900

    update data files handling

--- attck_utils.py
+++ attck_utils.py
@@ -1,1 +1,15 @@
-import pickle
+import os
+import pandas as pd
+
+def save_df_to_csv (path, filename: str, df: pd.DataFrame ):
+    """save dataframe as csv file within specified path
+
+    Args:
+        path (string)
+        df (pandas DataFrame)
+    """
+    if not filename.endswith (".csv"): name += ".csv"
+    
+    output_folder = os.makedirs (path, exist_ok= True)
+    output_file = os.path.join(output_folder, filename)
+    df.to_csv (output_file, index= False)
