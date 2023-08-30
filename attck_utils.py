5322c3a01635fbf7988658ec5542d91df7ef5b9a
HieuCVu_1050 <43056834+Hieu1050@users.noreply.github.com>
2023-08-30 11:11:57 +0900

    help functions to collect and save MITRE ATTCK file

--- attck_utils.py
+++ attck_utils.py
@@ -13,3 +13,18 @@ def save_df_to_csv (path, filename: str, df: pd.DataFrame ):
     os.makedirs (path, exist_ok= True)
     output_file = os.path.join(path, filename)
     df.to_csv (output_file, index= False)
+    
+def batch_save_df_to_csv (file_name_dfs: dict, path):
+    """
+    save the DataFrames stored in a dict as csv file. The filenames are the keys in the dict
+    """
+    for key in file_name_dfs.keys():
+        save_df_to_csv (
+            path = path,
+            filename = key,
+            df = file_name_dfs[key]
+        )
+    
+    print ("files saved to", path)
+    for key in file_name_dfs.keys():
+        print (key, ".csv")
--- data_collect_preprocess/data_collect_v1_0.py
+++ data_collect_preprocess/data_collect_v1_0.py
@@ -4,23 +4,24 @@ import pandas as pd
 
 import sys
 import os
+from ..attck_utils import save_df_to_csv
 
 # Get the root directory of the project
 root_folder = os.path.dirname(os.path.dirname(os.path.abspath('__file__')))
 
-file_path = os.path.join(root_folder, 'data', 'enterprise-attack.json')
+mitre_attck_file_path = os.path.join(root_folder, 'data', 'enterprise-attack.json')
 
 
-def data_collect(file_path = file_path):
-    
-    attackdata = MemoryStore ()
-    attackdata.load_from_file (file_path )
+def data_collect(file_path = mitre_attck_file_path):
     """
-    collect_data v1.0
+    v1.0
     
     reads 'enterprise-attack.json' from `path`
     
     """
+    
+    attackdata = MemoryStore ()
+    attackdata.load_from_file (file_path )
     techniques_data = stixToDf.techniquesToDf(attackdata, "enterprise-attack")
     groups_data = stixToDf.groupsToDf (attackdata)
     
@@ -31,3 +32,29 @@ def data_collect(file_path = file_path):
     groups_software_df = groups_data['associated software']
         
     return techniques_df, techniques_mitigations_df, groups_df, groups_techniques_df, groups_software_df
+
+data_dir = os.path.join(root_folder, 'data')
+
+def data_collect_and_save(data_dir = data_dir):
+    """
+    v 1.0
+    save these following DataFrames as csv in specifed path:
+        techniques_df, 
+        techniques_mitigations_df, 
+        groups_df, 
+        groups_techniques_df, 
+        groups_software_df
+    
+    """
+    techniques_df, techniques_mitigations_df, groups_df, groups_techniques_df, groups_software_df = data_collect()
+    
+    dfs = {
+    "techniques_df" : techniques_df,
+    "techniques_mitigations_df" : techniques_mitigations_df,
+    "groups_df": groups_df,
+    "groups_techniques_df" : groups_techniques_df,
+    "groups_software_df" : groups_software_df,
+    }
+    
+    
+    
